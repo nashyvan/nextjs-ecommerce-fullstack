b8b0e66a08889633b2a22c825b553e3adec96972
yariknashivan <yarik.nashivan@gmail.com>
2023-06-01 12:42:42 +0300

    admin basic design

--- ecommerce-admin/pages/index.js
+++ ecommerce-admin/pages/index.js
@@ -1,22 +1,19 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="bg-blue-900 v-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            onClick={() => signIn("google")}
-            className="bg-white p-2 rounded-lg px-4"
-          >
-            Login with Google
-          </button>
+  return (
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt="image" className="w-6 h-6" />
+          <span className="px-2">{session?.user?.name}</span>
         </div>
       </div>
-    );
-  }
-
-  return <div>logged in as {session.user.email}</div>;
+    </Layout>
+  );
 }
